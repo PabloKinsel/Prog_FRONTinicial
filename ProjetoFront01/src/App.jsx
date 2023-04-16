b608c83a661c5f6a8b655cd758ae1947894daefe
PabloKinsel <pabloyurik@gmail.com>
2023-04-16 10:48:56 -0300

    Atualizado

--- ProjetoFront01/src/App.jsx
+++ ProjetoFront01/src/App.jsx
@@ -1,21 +1,28 @@
-import './global.css';
 import style from "./App.module.css";
 
+import { CardPerson } from "./components/cardPerson";
+
+
 export function App() {
-  
+  const listStack = [
+    { Tec: "JavaScript", color:""},
+    { Tec: "HTML", color:""},
+    { Tec: "CSS" , color:""},
+    { Tec: "Typescript" , color:""},
+    { Tec: "React" , color:""},
+  ];
+
   return (
-    <article className={style.container}>
-      <div className={style.card}>
-      <img 
-      src="https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=80" alt='Cover'/>
-      <h1>Cover</h1>
-      <h1>Avatar</h1>
-      <h1>Descrição / Nome</h1>
-      <h1>Stack</h1>
-      </div>
-    </article>
-    
-  )
+    <div className={style.container}>
+      <CardPerson
+        cover="https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=80"
+        avatar="https://github.com/PabloKinsel.png"
+        name="Pablo Y Kinsel"
+        office="Web Developer"
+        stacks={listStack}
+      />
+    </div>
+  );
 }
 
 //export default App    Fica mais facil de chamar um funcão
